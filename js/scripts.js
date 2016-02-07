2bc720e3c6d80c2afebf0354b1fa5d879d8a32ea
Shruti Shankar <shruti07shankar@gmail.com>
2016-02-06 21:24:35 -0500

    Update site

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
 "use strict";
 
+/**
+ * This smooth scrolls the page to the selected link in the navigation anchor.
+ */
+$('.anchor').on('click', function(e){
+    e.preventDefault();
+
+    var targetId = $(this).data().id;
+    var target = $("#"+targetId);
+
+    $('html, body').stop().animate({
+        scrollTop: target.offset().top
+    }, 1000);
+});
+
+$('.project-link').on('click', function(e){
+    e.preventDefault();
+
+    var page = $(this).data().link;
+    document.location = page;
+});
